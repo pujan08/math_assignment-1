d6b3b57bc30cac7f5aa92090183e34f21a3cb557
pujan08 <bhattpujan08@gmail.com>
2024-05-17 20:09:30 -0400

    Add test and value assignment

---
+++ ex1_test (1).py
@@ -0,0 +1,3 @@
+
+if __name__ == "__main__":
+    test_hi_my_name_is()
---
+++ write_your_name.py
@@ -0,0 +1,3 @@
+def hi_my_name_is():
+    return "Pujan Bhatt" # Write your name inside the quotation marks
+value = hi_my_name_is() if hi_my_name_is() is not None else "No name provided"
